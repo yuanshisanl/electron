8361f592275bd9fe9875f1c34e635dfafceb5ceb
Milan Burda <milan.burda@gmail.com>
2019-04-23 21:28:37 +0200

    refactor: remove obscure enable/disable logic for remote module

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -2076,13 +2076,10 @@ v8::Local<v8::Value> WebContents::GetLastWebPreferences(
 }
 
 bool WebContents::IsRemoteModuleEnabled() const {
-  if (web_contents()->GetVisibleURL().SchemeIs("chrome-devtools")) {
-    return false;
-  }
   if (auto* web_preferences = WebContentsPreferences::From(web_contents())) {
     return web_preferences->IsRemoteModuleEnabled();
   }
-  return true;
+  return false;
 }
 
 v8::Local<v8::Value> WebContents::GetOwnerBrowserWindow() const {
